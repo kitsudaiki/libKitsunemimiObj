ec67e039c91eea8c81c549d88ce8473bdbf100f7
Tobias Anker <tobias.anker@kitsunemimi.moe>
2020-04-25 21:42:49 +0200

    update converter for the new objects

--- src/obj_creator.cpp
+++ src/obj_creator.cpp
@@ -39,6 +39,14 @@ ObjCreator::create(const ObjItem &input)
         result += std::to_string(input.vertizes.at(i).z) + "\n";
     }
 
+    // convert textures
+    for(uint32_t i = 0; i < input.textures.size(); i++)
+    {
+        result += "vt ";
+        result += std::to_string(input.textures.at(i).x) + " ";
+        result += std::to_string(input.textures.at(i).y) + "\n";
+    }
+
     // convert normals
     for(uint32_t i = 0; i < input.normals.size(); i++)
     {
@@ -46,6 +54,24 @@ ObjCreator::create(const ObjItem &input)
         result += std::to_string(input.normals.at(i).x) + " ";
         result += std::to_string(input.normals.at(i).y) + " ";
         result += std::to_string(input.normals.at(i).z) + "\n";
+    }
+
+    // convert points
+    for(uint32_t i = 0; i < input.points.size(); i++)
+    {
+        result += "p " + std::to_string(input.points.at(i)) + "\n";
+    }
+
+    // convert lines
+    for(uint32_t i = 0; i < input.lines.size(); i++)
+    {
+        result += "l";
+        for(uint32_t j = 0; j < input.lines.at(i).size(); j++)
+        {
+            const uint32_t id = input.lines.at(i).at(j);
+            result += " " + std::to_string(id);
+        }
+        result += "\n";
     }
 
     // convert faces
